6462e9621566afd7e4cb0aac8188ffb4218844cf
sota.goto-a <sota.goto@rakuten.com>
2023-01-20 15:21:32 +0900

    Added the functionality to print all the cards

---
+++ deck.go
@@ -0,0 +1,28 @@
+package main 
+
+import "fmt"
+
+// Create a new type of deck which is a slice of strings
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := [4]string{"❤️", "♦️", "♠️", "♣️"}
+	cardValues := [12]string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+value)
+		}
+	}
+	return cards
+}
+
+// d   : The actual copy of the deck we're working with is available in the functi as a variable called "d". d -> "cards" in this case.
+// deck: Every variable of type "deck" can call this function on itself.
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
