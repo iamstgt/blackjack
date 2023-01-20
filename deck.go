5082d2bbacda1765556864938da219541cb33e60
sota.goto-a <sota.goto@rakuten.com>
2023-01-20 15:58:35 +0900

    Added the functionality to deal the cards for player, dealer and remains

--- deck.go
+++ deck.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-// Create a new type of deck which is a slice of strings
+// Create a new type of deck which is a slice of strings.
 type deck []string
 
 func newDeck() deck {
@@ -25,4 +25,9 @@ func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
 	}
+}
+
+// This is not a receiver function. 
+func deal(d deck, handSize int) (deck, deck, deck) {
+	return d[:handSize], d[handSize:handSize+2], d[handSize+2:]
 }
--- main.go
+++ main.go
@@ -1,6 +1,22 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	// Call the function called "newDeck" returning cards whose type is "deck".
 	cards := newDeck()
-	cards.print()
+
+	// // Call the receiver function called "print".
+	// cards.print()
+
+	// Call the function to deal cards for player, dealer and remains.
+	playerHand, dealerHand, remainingCards := deal(cards, 2)
+	fmt.Println("--Player Hand--")
+	playerHand.print()
+	fmt.Println("--Dealer Hand--")
+	dealerHand.print()
+	fmt.Println("--Remaining Cards--")
+	remainingCards.print()
 }
